767373946c42a6c310a4908f1322b17f5f256833
Diego <diego@ensolvers.com>
2022-05-19 13:29:24 -0300

    Corrections to ebs deploy script

--- deploy-spring-boot-app-to-ebs.sh
+++ deploy-spring-boot-app-to-ebs.sh
@@ -2,23 +2,31 @@
 set -eu
 
 BASEDIR=$(dirname "$0")
-APP_DIR=${1-""}
+MODULE_LIST=("$1")
 AWS_ENV_CONFIGURATION=${2-""}
 
-if [ "$AWS_ENV_CONFIGURATION" != "-ignore-aws-env" ]; then
+if [ "$AWS_ENV_CONFIGURATION" != "-ignore-aws-vars" ]; then
   $BASEDIR/utils/check-aws-env.sh
 fi
 
-if [ -n "$APP_DIR" ]
-then
-  cd $APP_DIR
-fi
+# gets the modules
+SIZE=${#MODULE_LIST[*]}
 
-echo "IMPORTANT: remember that in default config, the branch that you are deploying from is linked to the environment to which the app will be deployed"
-echo "YOUR BRANCH IS: [$(git branch --show-current)]"
+for (( i=0; i<SIZE; i++ ))
+  do
 
-echo "[$(date)] Building app..."
-mvn clean install spring-boot:repackage -DskipTests
+  MODULE_NAME=${MODULE_LIST[i]}
 
-echo "[$(date)] Deploying to Elastic Beanstalk..."
-eb deploy --staged --verbose 
+  echo "Building module ${MODULE_NAME}"
+  echo "IMPORTANT: remember that in default config, the branch that you are deploying from is linked to the environment to which the app will be deployed"
+  echo "YOUR BRANCH IS: [$(git branch --show-current)]"
+
+  echo "[$(date)] Building app..."
+  mvn clean install spring-boot:repackage -DskipTests -pl :$MODULE_NAME -am
+
+  echo "[$(date)] Deploying to Elastic Beanstalk..."
+  cd "modules/$MODULE_NAME"
+  eb deploy --staged --verbose
+
+  cd -
+done
