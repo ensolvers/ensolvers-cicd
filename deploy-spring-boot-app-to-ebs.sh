e6440bd84a9ebd6e1931f1db608eac624c2fcc17
Diego <diego@ensolvers.com>
2022-05-20 10:51:58 -0300

    Added slack notifications

--- deploy-spring-boot-app-to-ebs.sh
+++ deploy-spring-boot-app-to-ebs.sh
@@ -4,6 +4,10 @@ set -eu
 BASEDIR=$(dirname "$0")
 MODULE_LIST=("$1")
 AWS_ENV_CONFIGURATION=${2-""}
+TIME_DEPLOY=$(date +%s)
+
+#load utils
+source "$BASEDIR/utils/slack_notification.sh"
 
 if [ "$AWS_ENV_CONFIGURATION" != "-ignore-aws-vars" ]; then
   $BASEDIR/utils/check-aws-env.sh
@@ -11,6 +15,14 @@ fi
 
 # gets the modules
 SIZE=${#MODULE_LIST[*]}
+
+if [ -z "$TAG" ]; then
+  slack_notification "[${ENV^^}] [$(date +"%H:%M:%S") UTC-3] - Deploying ${BRANCH}"
+else
+  slack_notification "[${ENV^^}] [$(date +"%H:%M:%S") UTC-3] - Deploying from tag ${TAG}"
+fi
+
+slack_notification "[${ENV^^}] [$(date +"%H:%M:%S") UTC-3] - Building for ${MODULE_LIST[*]}"
 
 for (( i=0; i<SIZE; i++ ))
   do
@@ -24,9 +36,22 @@ for (( i=0; i<SIZE; i++ ))
   echo "[$(date)] Building app..."
   mvn clean install spring-boot:repackage -DskipTests -pl :$MODULE_NAME -am
 
+  ERROR_CODE=$?
+
+  if [ "$ERROR_CODE" -gt 0 ]
+  then
+    slack_notification "[${ENV^^}] [$(date +"%H:%M:%S") UTC-3] - Building Failed - Error Code: $ERROR_CODE"
+    { exit 1; }
+  fi
+
   echo "[$(date)] Deploying to Elastic Beanstalk..."
   cd "modules/$MODULE_NAME"
   eb deploy --staged --verbose
 
+  slack_notification "[${ENV^^}] [$(date +"%H:%M:%S") UTC-3] - Module \`$MODULE_NAME\` deployed to EBS"
+
   cd -
 done
+
+TIME_FINAL=`expr $(date +%s) - $TIME_DEPLOY`
+slack_notification "[${ENV^^}] [$(date +"%H:%M:%S") UTC-3] - Deploy Finished - Took $((TIME_FINAL /60/60)) hours, $(((TIME_FINAL /60) % 60)) minutes, $((TIME_FINAL % 60)) seconds."
