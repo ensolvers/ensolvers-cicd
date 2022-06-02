dee4fbcf634a2fc614dc1ac9216ad71ebff48a91
Diego <diego@ensolvers.com>
2022-06-02 11:46:06 -0300

    Minor fix

--- utils/codebuild_environment_configuration_commands.sh
+++ utils/codebuild_environment_configuration_commands.sh
@@ -1,12 +1,10 @@
-#!/bin/bash
-
 set -e
 
 BASEDIR=$(dirname "$0")
 
 #load utils
-source "$BASEDIR/slack_notification.sh"
-source "$BASEDIR/error_handler_slack_message.sh"
+. "$BASEDIR/slack_notification.sh"
+. "$BASEDIR/error_handler_slack_message.sh"
 
 # Configure code base and submodules
 slack_notification "[${ENV^^}] [$(date +"%H:%M:%S") UTC] - Deploy started by \`${CODEBUILD_INITIATOR}\`"
