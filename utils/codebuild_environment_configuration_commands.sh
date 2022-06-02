2a710868190463578ff3b464fc47f028df8f8ba2
Diego <diego@ensolvers.com>
2022-06-02 11:58:27 -0300

    Moved env variables declaration from script to buildpec

--- utils/codebuild_environment_configuration_commands.sh
+++ utils/codebuild_environment_configuration_commands.sh
@@ -3,17 +3,11 @@ set -e
 BASEDIR=$(dirname "$0")
 
 #load utils
-. "$BASEDIR/slack_notification.sh"
-. "$BASEDIR/error_handler_slack_message.sh"
+source "$BASEDIR/slack_notification.sh"
+source "$BASEDIR/error_handler_slack_message.sh"
 
 # Configure code base and submodules
 slack_notification "[${ENV^^}] [$(date +"%H:%M:%S") UTC] - Deploy started by \`${CODEBUILD_INITIATOR}\`"
-
-echo "Configuring extra vars"
-export MAVEN_CONFIG=''
-export _JAVA_OPTIONS=-Xmx12g
-export ROOT_DIR=$PWD
-echo "Extra vars configured"
 
 echo "Configuring branch/tag"
 if [ -z "$TAG" ]; then
