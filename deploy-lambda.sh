afe901cab532fa83e933adafe6ceec381e4135a3
José Matías Rivero <jose.matias.rivero@gmail.com>
2023-01-01 22:44:17 -0300

    Added log to lambda upload fn

--- deploy-lambda.sh
+++ deploy-lambda.sh
@@ -4,6 +4,7 @@ set -eu
 function_code_root=$1
 function_name=$2
 
+echo "Deploying function in $function_code_root to $function_name"
 cd $function_code_root
 rm function.zip
 zip -r function.zip *.js *.json node_modules
