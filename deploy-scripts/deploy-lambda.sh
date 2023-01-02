910691ea9523463c1bd9adb654ce4d2a911390b9
José Matías Rivero <jose.matias.rivero@gmail.com>
2023-01-01 23:04:28 -0300

    deploy-lambda.sh: avoid failing in zip does not exist

--- deploy-scripts/deploy-lambda.sh
+++ deploy-scripts/deploy-lambda.sh
@@ -6,7 +6,7 @@ function_name=$2
 
 echo "Deploying function in $function_code_root to $function_name"
 cd $function_code_root
-rm function.zip
+rm -rf function.zip
 zip -r function.zip *.js *.json node_modules
 aws lambda update-function-code --function-name $function_name --zip-file fileb://function.zip
-rm function.zip
+rm -rf function.zip
