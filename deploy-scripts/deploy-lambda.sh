430092d5d38569a9703bc6ff730c88844000a985
José Matías Rivero <jose.matias.rivero@gmail.com>
2023-01-05 17:59:15 -0300

    Included all file in lambda-deploy.sh

--- deploy-scripts/deploy-lambda.sh
+++ deploy-scripts/deploy-lambda.sh
@@ -7,6 +7,6 @@ function_name=$2
 echo "Deploying function in $function_code_root to $function_name"
 cd $function_code_root
 rm -rf function.zip
-zip -r function.zip *.js *.json node_modules
+zip -r function.zip *
 aws lambda update-function-code --function-name $function_name --zip-file fileb://function.zip
 rm -rf function.zip
