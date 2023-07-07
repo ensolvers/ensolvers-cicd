1d6e32972b66c9ebc0c5bb2fd87cb297152792b0
Hipolito Alanis <hipolitoalanis@gmail.com>
2023-07-07 17:30:22 -0300

    Adds echo for parameters in maven

--- utils/gatling-tests.sh
+++ utils/gatling-tests.sh
@@ -4,6 +4,10 @@ TEST_DIR="$1"
 SIMULATION_CLASS="$2"
 GATLING_ARGS="$3"
 
+echo "Dir: $TEST_DIR"
+echo "Class: $SIMULATION_CLASS"
+echo "Args: $GATLING_ARGS"
+
 cd $TEST_DIR
 
 mvn gatling:test -Dgatling.simulationClass=$SIMULATION_CLASS  "$GATLING_ARGS"
