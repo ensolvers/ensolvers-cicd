1513791e6972c889b3477d6d157535f9d28a83f8
Hipolito Alanis <hipolitoalanis@gmail.com>
2023-07-24 17:51:17 -0300

    Adds perftest script

--- utils/gatling-tests.sh
+++ utils/gatling-tests.sh
@@ -3,6 +3,7 @@
 TEST_DIR="$1"
 SIMULATION_CLASS="$2"
 GATLING_ARGS="$3"
+REPORT_FILE_S3_URL="$4"/reports/$SIMULATION_CLASS-$(date +%s).html
 
 echo "Dir: $TEST_DIR"
 echo "Class: $SIMULATION_CLASS"
@@ -11,3 +12,5 @@ echo "Args: $GATLING_ARGS"
 cd $TEST_DIR
 
 mvn gatling:test -Dgatling.simulationClass=$SIMULATION_CLASS $GATLING_ARGS
+
+aws s3 cp target/gatling/*.html "$REPORT_FILE_S3_URL"
