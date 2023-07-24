19deae4c2a898488181a6775290061a88512fc02
Hipolito Alanis <hipolitoalanis@gmail.com>
2023-07-24 19:34:01 -0300

    Fixes on s3 reference for bucket

--- utils/gatling-tests.sh
+++ utils/gatling-tests.sh
@@ -3,11 +3,12 @@
 TEST_DIR="$1"
 SIMULATION_CLASS="$2"
 GATLING_ARGS="$3"
-REPORT_FILE_S3_URL="$4"/$(echo "SIMULATION_CLASS" | awk -F "." '{print $NF}')/$(date +%s).html
+REPORT_FILE_S3_URL="s3://$4/$(echo "SIMULATION_CLASS" | awk -F "." '{print $NF}')/$(date +%s).html"
 
 echo "Dir: $TEST_DIR"
 echo "Class: $SIMULATION_CLASS"
 echo "Args: $GATLING_ARGS"
+echo "Report File: $REPORT_FILE_S3_URL"
 
 cd $TEST_DIR
 
