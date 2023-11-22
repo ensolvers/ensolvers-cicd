8708bafd5678ca64a7dceefc7af70e83d908b033
José Matías Rivero <matias@ensolvers.com>
2023-11-22 16:07:38 -0300

    Git hooks: automatic staging of formatted files

--- scripts/git/install-git-hooks.sh
+++ scripts/git/install-git-hooks.sh
@@ -1,6 +1,6 @@
 # register auto-formatting git hook
-echo 'npx prettier --write "modules/frontend/src/**/*.{js,ts,jsx,tsx}"' > .git/hooks/pre-commit
-echo 'mvn clean process-resources' >> .git/hooks/pre-commit
+echo 'npx prettier --write "modules/frontend/src/**/*.{js,ts,jsx,tsx}"; git add .' > .git/hooks/pre-commit
+echo 'mvn clean process-resources; git add .' >> .git/hooks/pre-commit
 
 # ensure that hook is executable
 chmod +x .git/hooks/pre-commit
