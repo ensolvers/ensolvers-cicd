fe5ebe26881e638c47cae7b456184c96eb1b71cb
José Matías Rivero <matias@ensolvers.com>
2023-11-23 08:41:33 -0300

    Improvements in git hooks

--- scripts/git/install-git-hooks.sh
+++ scripts/git/install-git-hooks.sh
@@ -1,3 +1,5 @@
+echo "Ensuring git hook scripts..."
+
 # register auto-formatting git hook
 echo 'npx prettier --write "modules/frontend/src/**/*.{js,ts,jsx,tsx}"; git add .' > .git/hooks/pre-commit
 echo 'mvn clean process-resources; git add .' >> .git/hooks/pre-commit
