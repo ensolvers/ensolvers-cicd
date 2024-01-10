91ea9de33162b35a78a616e5628d488011fb4e6e
Hipolito Alanis <hipolitoalanis@gmail.com>
2024-01-09 22:09:34 -0300

    Adds option for chromium image

---
+++ docker/docker-jar-runner-with-chromium/deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+repo=$1
+timezone="${2:-UTC}"
+
+if [[ -z $repo ]]; then
+    echo "Usage $0 <repo> <timezone>, e.g.: $0 238750409794.dkr.ecr.us-east-1.amazonaws.com/jar-runner UTC"
+    exit 1;
+fi
+
+tag=$(git rev-parse HEAD)
+
+docker build . -t jar-runner:$tag --build-arg timezone=$timezone
+docker tag jar-runner:$tag $repo:$tag
+docker push $repo:$tag
