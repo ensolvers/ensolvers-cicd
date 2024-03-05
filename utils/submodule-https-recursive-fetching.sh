08904ccb54d89d6817b4905f5520dccfb1275e6f
José Matías Rivero <matias@ensolvers.com>
2024-03-05 16:52:26 -0300

    Improve HTTPS recursive fetching script

--- utils/submodule-https-recursive-fetching.sh
+++ utils/submodule-https-recursive-fetching.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+root_dir="$1"
+
 update_submodules() {
     cd "$1" || exit
+    echo "Replacing SSH to HTTPS on [$1/.gitmodules]"
     sed -i "s|git@github.com:|https://github.com/|g" .gitmodules
     git submodule update --init
 }
 
-find . -type f -name ".gitmodules" | while read -r file; do
+find "$root_dir" -type f -name ".gitmodules" | while read -r file; do
     dir=$(dirname "$file")
     echo "Fetching submodule [$dir]..."
     update_submodules "$dir"
