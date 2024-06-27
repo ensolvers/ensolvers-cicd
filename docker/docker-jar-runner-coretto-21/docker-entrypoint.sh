f58563e8b114324768301f967907b30a5630640a
Fede <federico.c@ensolvers.com>
2024-06-27 17:02:58 -0300

    Fixed coretto-21 image entrypoint

--- docker/docker-jar-runner-coretto-21/docker-entrypoint.sh
+++ docker/docker-jar-runner-coretto-21/docker-entrypoint.sh
@@ -7,9 +7,9 @@ if [[ ! -z $JAR_FILE_S3_URL ]]; then
     
     echo "Running jar..."
     if [[ ! -z $NEW_RELIC_LICENSE_KEY ]]; then
-        NEW_RELIC_VERSION="$(java -jar /newrelic.jar -v)"
+        NEW_RELIC_VERSION="$(java -jar /opt/newrelic/newrelic.jar -v)"
         echo "Running with New Relic version $NEW_RELIC_VERSION, license_key: [$NEW_RELIC_LICENSE_KEY], app_name: [$NEW_RELIC_APP_NAME]"
-        exec java -javaagent:/newrelic.jar -Dnewrelic.config.license_key=$NEW_RELIC_LICENSE_KEY -Dnewrelic.config.app_name=$NEW_RELIC_APP_NAME $JVM_PARAMS -jar /app.jar
+        exec java -javaagent:/opt/newrelic/newrelic.jar -Dnewrelic.config.license_key=$NEW_RELIC_LICENSE_KEY -Dnewrelic.config.app_name=$NEW_RELIC_APP_NAME $JVM_PARAMS -jar /app.jar
     else
         exec java $JVM_PARAMS -jar /app.jar
     fi
